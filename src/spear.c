243604f5f53c21e8c2248a3e28fc88f24c178463
Nagasato Kazuya <nagasato.k.ac@m.titech.ac.jp>
2019-06-21 00:30:31 +0900

    submit

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p (int x) {
+  int sum = 0;
+  if (x == 0) return 0;
+  for (int i = 0; i < n; i++){      //長さxの棒がsum本だけ作れる
+    sum += A[i] / x;
+  }
+  return sum < k;    //p(x)を非減少単調数列とする(作れるなら0,作れないなら1を返す)
+}
 
 int main(){
   int i, lb, ub;
@@ -11,6 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+
+  lb = -1;    //出力の最小値-1
+  ub = 1000000001;    //出力の最大値+1
+
+  while (ub - lb > 1) {
+    int m = (ub + lb) / 2;
+    if (p(m)) ub = m;
+    else lb = m;
+  }
+
+  printf("answer is %d\n", ub-1);
 
 
   return 0;
